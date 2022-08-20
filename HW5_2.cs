d3d0caa7e7a8023eecdea719aec172cc1a16c778
IVANNNNN <gravemail@yandex.ru>
2022-08-20 15:44:48 +0300

    Homework 5_2

---
+++ HW5_2.cs
@@ -0,0 +1,48 @@
+﻿int [] createArray(int length)
+{
+    var random = new Random ();
+    var result = new int [length];
+    for (long i = 0; i < result.Length; i++)
+    {
+        result [i] = random.Next(-99,100);
+    }
+    return result;
+}
+
+void printArray (int [] array)
+{
+   Console.Write ("[");
+   
+    for (var i = 0; i < array.Length; i ++)
+    {   
+        Console.Write (array[i]);
+         if (i < array.Length - 1)
+         Console.Write (", ");
+        
+    }
+   Console.Write ("]");
+   Console.WriteLine();
+}
+
+int sumNumb (int [] array) {   
+    int sumNumbs = 0;
+    for (int i = 0; i <= array.Length - 1; i++) {   
+        if (i%2 == 1) sumNumbs = sumNumbs + array[i];      
+    }
+    return sumNumbs;
+}   
+
+Console.Write ("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+var array = createArray(length);
+Console.WriteLine("Сгенерировали массив");
+
+printArray(array);
+
+Console.WriteLine();
+
+int result = sumNumb(array);
+
+Console.Write("Сумма чисел на нечетных позициях: " + result);
